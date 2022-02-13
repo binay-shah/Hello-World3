e7887c9474c7ea36c227e4064f6c05193c0d4121
Binay Shah <binayshah@Binays-MacBook-Pro.local>
2022-02-13 08:04:08 +0545

    added log statement

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
+
+    public final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count  = 10;
+
+        Log.d(TAG, String.valueOf(count));
+
 
 
     }
